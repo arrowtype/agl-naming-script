ef833f47bcfd57b17dbafe9669821213bfb199f2
arrowtype <arrowtypeco@gmail.com>
2021-09-08 21:43:52 -0400

    improve report output

--- script/set-prod-names.py
+++ script/set-prod-names.py
@@ -6,6 +6,7 @@
         python3 script/set-prod-names.py -u sources
 
     Current assumptions:
+    - Glyphs have only one Unicode value each (it is possible, but rare, for glyphs to have multiple Unicodes pointing to them)
     - Fonts do not have glyphs that are ligated (which would need names like 'uni20AC0308', mapped to the string U+20AC U+0308)
     
 """
@@ -24,6 +25,7 @@ def findProdNames(font):
     # setting up report to store changes
     report = {}
     report["Production names to set"] = {}
+    report["Prod Names with Codepoints"] = {}
     report["Glyphs with already-correct naming"] = []
     report["Glyphs with no Unicode value"] = []
 
@@ -39,22 +41,29 @@ def findProdNames(font):
                 # if not, add it to a dict to correct below
                 else:
                     report["Production names to set"][glyph.name] = agl.UV2AGL[glyph.unicodes[0]]
+                    report["Prod Names with Codepoints"][glyph.unicodes[0]] = (glyph.name, agl.UV2AGL[glyph.unicodes[0]])
 
-            # catch if name doesn’t match a name in AGL, then give a "uni____" name
+            # catch if name doesn’t match a name in AGL, then give a "uni0000" name
             # formatting help: https://gist.github.com/arrowtype/713dad14fe9a574d58d1aab61ba9b2f0#gistcomment-3234771
             except KeyError:
                 # add it to a dict to correct below
                 report["Production names to set"][glyph.name] = f'uni{glyph.unicodes[0]:0>4X}'
+                report["Prod Names with Codepoints"][glyph.unicodes[0]] = (glyph.name, f'uni{glyph.unicodes[0]:0>4X}')
 
                 # if hex is above the range 0000–FFFF, it must be named differently, with a u000000 format
                 if glyph.unicodes[0] > 65535:
                     report["Production names to set"][glyph.name] = f'u{glyph.unicodes[0]:0>6X}'
+                    report["Prod Names with Codepoints"][glyph.unicodes[0]] = (glyph.name, f'u{glyph.unicodes[0]:0>6X}')
 
-                # TODO? maybe check for ligated glyphs? These should have an underscore before the first period, e.g. 'uni20AC0308', mapped to the string U+20AC U+0308
+                # TODO? maybe check for ligated glyphs? In working glyphs, these should have an underscore before the first period, and then should turn into a format like 'uni20AC0308', mapped to the string U+20AC U+0308
 
         # if glyph has no Unicode value
         if glyph.unicodes == ():
             report["Glyphs with no Unicode value"].append(glyph.name)
+
+        # just to catch the possibility of glyphs with more than one Unicode (rare, but possible)
+        if len(glyph.unicodes) > 1:
+            raise Exception(f"Glyph /{glyph.name} has multiple Unicodes. This is not supported by this script. You should either upgrade the script, or else break up the glyph into one glyph per Unicode.")
 
     return report
 
@@ -79,33 +88,33 @@ def saveReport(path, report):
     *path* is a `string` of the path to write to
     """
 
-    seperator = "\n_________________________"
+    seperator = "\n---------------------------------------------------"
 
-    final_report = [f"{path.split('.')[0]}.ufo", seperator, "Production names set:"]
-    if report["Production names to set"]:
-        for oldName, newName in report["Production names to set"].items():
-            final_report.append(f"\t{oldName.ljust(31)}→ {newName}")
+    final_report = ["PRODUCTION NAMING REPORT\n", f"{path.split('.')[0]}.ufo", seperator, "Production names set:\n"]
+    final_report.append(f"    {'CODEPOINT'.rjust(9)} : {'WORKING NAME'.ljust(31)} → PROD NAME\n")
+    if report["Prod Names with Codepoints"]:
+        for codepoint, oldNameNewName in sorted(report["Prod Names with Codepoints"].items()):
+            final_report.append(f"    {str(codepoint).rjust(9)} : {oldNameNewName[0].ljust(31)} → {oldNameNewName[1]}")
     else:
         final_report.append("No names needed setting!")
 
     final_report.append(seperator)
     final_report.append("Glyphs with already-correct naming:")
     if report["Glyphs with already-correct naming"]:
-        for name in report["Glyphs with already-correct naming"]:
-            final_report.append(f"\t{name}")
+        final_report.append("    " + " ".join([name for name in sorted(report["Glyphs with already-correct naming"])]))
     else:
         final_report.append("No glyphs had already-correct names.")
 
     final_report.append(seperator)
     final_report.append("Glyphs with no Unicode value:")
     if report["Glyphs with no Unicode value"]:
-        for name in report["Glyphs with no Unicode value"]:
-            final_report.append(f"\t{name}")
+        final_report.append("    " + " ".join([name for name in sorted(report["Glyphs with no Unicode value"])]))
     else:
         final_report.append("No glyphs had no unicode values.")
 
     with open(path, "w") as reportDoc:
         reportDoc.write("\n".join(final_report))
+        print(f"DONE! Results recorded to {path}")
 
 
 if __name__ == "__main__":
